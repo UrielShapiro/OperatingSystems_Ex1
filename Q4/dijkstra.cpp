649a971d7697551d8d5ae5fbd96e2a696fc28f5f
uriel <uriel1999@gmail.com>
2024-04-25 06:07:11 +0300

    Added comments and removed unneccery prints (comments are not done)

--- Q4/dijkstra.cpp
+++ Q4/dijkstra.cpp
@@ -1,86 +1,97 @@
 #include <iostream>
 #include <climits>
 #include <vector>
-#include <string>
 using namespace std;
-
+/*
+A function that receives a graph represented as an adjacency matrix (line by line) and stores it in a 2D vector.
+The function checks if the input is valid and exits the program if it is not.
+*/
 void input_graph(vector<vector<int>> &graph)
 {
-    size_t mat_len = SIZE_MAX;
-    for (size_t i = 0; i < mat_len; i++)
+    size_t mat_len = SIZE_MAX;                 // Setting the length of the matrix to the maximum value before the first iteration
+                                               // In order to let the user enter the amount of vertices,
+                                               // then limit the length of the matrix to that value.
+    for (size_t row = 0; row < mat_len; row++) // Looping through the rows of the matrix
     {
-        if (i == 1)
+        if (row == 1) // If the user has entered the first row, then the length of the matrix is known
         {
             mat_len = graph.at(0).size();
-            graph.resize(mat_len);
+            graph.resize(mat_len); // Resizing the matrix to the length of the matrix
         }
-        size_t j = 0;
-        bool run = true;
+        size_t col = 0;
         char input;
-        cout << "Enter the weight of the edge between vertice " << i << " to vertice " << j << ": " << endl;
-        cin.clear();
-        while ((input = cin.get()) != '\n' && run == true)
+        cout << "Enter the weight of the edges from vertice " << row << ": " << endl; // Prompting the user to enter the weights of the edges
+        cin.clear();                                                                  // Clearing the input buffer
+        while ((input = cin.get()) != '\n')                                           // Looping through the input until the user presses enter ('\n')
         {
-            cout << "Input was: " << input << endl;
-            if (input == '\n' || input == '\0' || input == EOF)
-            {
-                run = false;
-                break;
-            }
-            else if (input == ' ' || isalpha(input))
+            if (input == ' ' || isalpha(input)) // If the input is a space or a letter or NaN, then it is ignored.
             {
                 continue;
             }
-            if (j >= mat_len)
-            {
+            if (col >= mat_len) // If the amount of weights (edges) entered is more than the amount of vertices,
+            {                   // an error will be printed and the program exits.
                 cerr << "Error: the amount of weights entered is not equal to the amount of vertices!" << endl;
                 exit(1);
             }
-            int weight = input - '0';
-            if (weight < 0)
-            {
+            int weight = input - '0'; // Parsing the input to an integer
+            if (weight < 0)           // Negative weights are not allowed.
+            {                         // If a negative weight is entered, an error will be printed and the program exits.
                 cerr << "Error: weight must be a positive number! but it is: " << weight << endl;
                 exit(1);
             }
-            if (j == i && weight != 0)
+            if (col == row && weight != 0) // Checking if the distance between a vertice and itself is 0.
             {
-                cerr << "Error: the weight of the edge between vertice " << i << " and itself must be 0!" << endl;
+                cerr << "Error: the weight of the edge between vertice " << row << " and itself must be 0!" << endl;
                 exit(1);
             }
-            cout << "Weight was: " << weight << endl;
-            graph.at(i).push_back(weight);
-            j++;
-            cout << "j is now: " << j << endl;
+            graph.at(row).push_back(weight); // Storing the weight at graph[row][col]
+            col++;
         }
     }
 }
 
-// A utility function to print the constructed distance array
+/*
+A function to print the array which stores the shortest distance from the source to each vertice in the graph.
+*/
 void printSolution(vector<int> dist)
 {
-    cout << "Vertex \t Distance from Source" << endl;
+    cout << "Vertex\tDistance from Source" << endl;
     for (size_t i = 0; i < dist.size(); i++)
-        cout << i << " \t" << dist[i] << endl;
+    {
+        if (dist[i] == INT_MAX) // If dist[i] was not updated, then the vertice is not connected to the source.
+        {
+            cout << i << "\t"
+                 << "Not Connected" << endl;
+        }
+        else
+        {
+            cout << i << "\t" << dist[i] << endl;
+        }
+    }
 }
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
-int MinNeighbor(size_t V, vector<int> dist, vector<bool> converged)
+/*
+A function to find the neighbor with the minimum distance from the source.
+Assuming that that neighbor was not visited yet.
+*/
+int MinNeighbor(vector<int> dist, vector<bool> converged)
 {
-    // Initialize min value
     int min = INT_MAX, min_index;
 
-    for (size_t v = 0; v < V; v++)
-        if (converged[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-
-    return min_index;
+    for (size_t vertex_i = 0; vertex_i < dist.size(); vertex_i++) // Looping through the vertices
+        if (converged[vertex_i] == false && dist[vertex_i] <= min)
+        {
+            min = dist[vertex_i];   // The unvisited neighbor with the minimum distance from the source is stored in min.
+            min_index = vertex_i;   // Updating min_index to the index of the neighbor with the minimum distance from the source.
+        }
+    return min_index;               // After iterating through all the vertices, the index of the neighbor with the minimum distance from the source is returned.
 }
-
-bool relaxed(int u, int v, int weight, vector<int> dist)
+/*
+A function that checks if a vertex should be relaxed or not.
+*/
+bool shouldRelax(int u, int v, int weight, vector<int> dist)
 {
-    if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
+    if (dist[u] != INT_MAX && dist[u] + weight <= dist[v])
     {
         return true;
     }
@@ -98,8 +109,8 @@ void dijkstra(vector<vector<int>> graph, int src)
                             // included in shortest path tree or shortest distance from src to i is finalized
 
     // Initialize all distances as INFINITE and stpSet[] as false;
-    dist.assign(graph.size() - 1, INT_MAX);
-    converged.assign(graph.size() - 1, false);
+    dist.assign(graph.size(), INT_MAX);
+    converged.assign(graph.size(), false);
 
     // Distance of source vertex from itself is always 0
     dist.at((size_t)src) = 0;
@@ -110,7 +121,7 @@ void dijkstra(vector<vector<int>> graph, int src)
         // Pick the minimum distance vertex from the set of
         // vertices not yet processed. u is always equal to
         // src in the first iteration.
-        int u = MinNeighbor(graph.size(), dist, converged);
+        int u = MinNeighbor(dist, converged);
 
         // Mark the picked vertex as processed
         converged[u] = true;
@@ -122,8 +133,10 @@ void dijkstra(vector<vector<int>> graph, int src)
             // there is an edge from u to v, and total
             // weight of path from src to  v through u is
             // smaller than current value of dist[v]
-            if (!converged[v] && graph.at(u).at(v) && relaxed(u, v, graph.at(u).at(v), dist))
+            if (!converged[v] && graph[u][v] && shouldRelax(u, v, graph.at(u).at(v), dist))
+            {
                 dist[v] = dist[u] + graph[u][v];
+            }
     }
 
     // print the constructed distance array
