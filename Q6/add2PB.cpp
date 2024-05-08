25b3bcdc5d721c079bb249ce299a8877c084f317
Chaim Averbach <Chaymi99@gmail.com>
2024-05-08 14:59:22 +0300

    Made add2PB take command line arguments

--- Q6/add2PB.cpp
+++ Q6/add2PB.cpp
@@ -1,11 +1,16 @@
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
 
 const char *PB_FILEPATH = "phonebook.txt";
 
-bool add(std::string name, std::string num)
+int main(int argc, char **argv)
 {
+	char **args = (char **) malloc(sizeof(char *)*(argc + 1));
+	memcpy(args, argv, sizeof(char *)*argc);
+	args[argc] = NULL;
+
 	// we will emulate the command:
 	// echo $(name) , $(num) >> $(PB_FILEPATH)
 
@@ -13,21 +18,9 @@ bool add(std::string name, std::string num)
 	if (fork() == 0)
 	{
 		dup2(open(PB_FILEPATH, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH), STDOUT_FILENO);
-		execlp("echo", "echo", name.c_str(), ",", num.c_str(), (char *) NULL);	// execute the echo
+		execvp("echo", args);	// execute the echo
 
 	}
-	return true;
-}
-// TODO: make this take command line arguments instead of stdin input
-int main(void)
-{
-	std::string name, num;
-	std::cout << "Enter name: ";
-	std::getline(std::cin, name);
-	std::cout << "Enter number: ";
-	std::cin >> num;
-
-	add(name, num);
 
 	return 0;
 }
