f64bdc2985a3a4ef5483322dcd3ddc62f887c197
Chaim Averbach <Chaymi99@gmail.com>
2024-05-17 04:28:06 +0300

    Added documentation

--- Q6/findPhone.cpp
+++ Q6/findPhone.cpp
@@ -14,37 +14,58 @@ bool print_number(char *name)
 {
 	// we will emulate the command:
 	// grep $(name) < $(PB_FILEPATH) | cut -d, -f2 | xargs
+
 	int pipe_grep_to_cut[2], pipe_cut_to_xargs[2];
+	// create the first pipe
 	pipe(pipe_grep_to_cut);
+	// fork for grep
 	if (fork() == 0)
 	{
+		// close the read end of the pipe
 		close(pipe_grep_to_cut[PIPE_READ_END]);
+		// open the phonebook file for reading and dup it into stdin
 		dup2(open(PB_FILEPATH, O_RDONLY), STDIN_FILENO);
+		// dup the pipe write end into stdout
 		dup2(pipe_grep_to_cut[PIPE_WRITE_END], STDOUT_FILENO);
+		// execute the grep
 		execlp("grep", "grep", name, (char *) NULL);
 	}
 	
+	// create the second pipe
 	pipe(pipe_cut_to_xargs);
 	if (fork() == 0)
 	{
+		// close unused pipe ends
 		close(pipe_grep_to_cut[PIPE_WRITE_END]);
 		close(pipe_cut_to_xargs[PIPE_READ_END]);
+		// dup the first pipe into stdin
 		dup2(pipe_grep_to_cut[PIPE_READ_END], STDIN_FILENO);
+		// dup the second pipe into stdout
 		dup2(pipe_cut_to_xargs[PIPE_WRITE_END], STDOUT_FILENO);
+		// execute cut
 		execlp("cut", "cut", "-d,", "-f2", (char *) NULL);
 	}
-	int xargs_pid;
+	// close the unused pipe ends
 	close(pipe_grep_to_cut[PIPE_READ_END]);
 	close(pipe_grep_to_cut[PIPE_WRITE_END]);
+
+	// save the xargs pid
+	int xargs_pid;
+	// fork to run xargs
 	if ((xargs_pid = fork()) == 0)
 	{
+		// close the other end of the pipe
 		close(pipe_cut_to_xargs[PIPE_WRITE_END]);
+		// dup the read end into stdin
 		dup2(pipe_cut_to_xargs[PIPE_READ_END], STDIN_FILENO);
+		// execute xargs
 		execlp("xargs", "xargs", (char *) NULL);
-		exit(0);
 	}
+	// close all remaining pipe ends
 	close(pipe_cut_to_xargs[PIPE_READ_END]);
 	close(pipe_cut_to_xargs[PIPE_WRITE_END]);
+
+	// wait until xargs finishes, to not mix outputs
 	waitpid(xargs_pid, NULL, 0);
 	return true;
 }
