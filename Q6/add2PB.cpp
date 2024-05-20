7c02cbfa2dff82b46b65ed26ab2cf99463ba1594
Uriel Shapiro <uriel1999@gmail.com>
2024-05-20 15:53:11 +0300

    Added comments and made a print for names that are not found in the phonebook

--- Q6/add2PB.cpp
+++ Q6/add2PB.cpp
@@ -7,20 +7,48 @@ const char *PB_FILEPATH = "phonebook.txt";
 
 int main(int argc, char **argv)
 {
-	char **args = (char **) malloc(sizeof(char *)*(argc + 1));
-	memcpy(args, argv, sizeof(char *)*argc);
+	if (argc < 2)
+	{
+		std::cerr << "Error: No arguments provided" << std::endl;
+		return 1;
+	}
+
+	// Create a new array for the arguments to pass to execvp (need to add NULL at the end)
+	char **args = (char **)malloc(sizeof(char *) * (argc + 1));
+	if (args == NULL)
+	{
+		std::cerr << "Error allocating memory" << std::endl;
+		return 1;
+	}
+
+	memcpy(args, argv, sizeof(char *) * argc);
 	args[argc] = NULL;
 
 	// we will emulate the command:
 	// echo ...(args) >> $(PB_FILEPATH)
 
-	// fork to run `echo`
-	if (fork() == 0)
+	// open the phonebook file for writing and append (will create the file if it doesn't exist)
+	int fd = open(PB_FILEPATH, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd == -1)
 	{
-		dup2(open(PB_FILEPATH, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH), STDOUT_FILENO);
-		execvp("echo", args);	// execute the echo
-
+		strerror(errno);
+		std::cerr << "Error opening file" << std::endl;
+		return 1;
 	}
 
+	// Redirect stdout to the phonebook file
+	if (dup2(fd, STDOUT_FILENO) == -1)
+	{
+		strerror(errno);
+		std::cerr << "Error redirecting stdout" << std::endl;
+		return 1;
+	}
+
+	if(execvp("echo", args) == -1) // "echo" the arguments to the phonebook file (after stdout has been redirected)
+	{
+		strerror(errno);
+		std::cerr << "Error executing echo" << std::endl;
+		return 1;
+	}
 	return 0;
 }
--- Q6/findPhone.cpp
+++ Q6/findPhone.cpp
@@ -4,69 +4,159 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <cstring>
 
 const char *PB_FILEPATH = "phonebook.txt";
 
 #define PIPE_READ_END 0
 #define PIPE_WRITE_END 1
 
+/**
+ * @brief Print the phone number of a person with the given name
+ * @return True if the name was found, false otherwise
+ */
 bool print_number(char *name)
 {
 	// we will emulate the command:
 	// grep $(name) < $(PB_FILEPATH) | cut -d, -f2 | xargs
 
+	// xargs pipe - to remove the newline character and unneccery spaces
+	// cut pipe - to extract the phone number
 	int pipe_grep_to_cut[2], pipe_cut_to_xargs[2];
+
 	// create the first pipe
-	pipe(pipe_grep_to_cut);
+	if (pipe(pipe_grep_to_cut) == -1)
+	{
+		strerror(errno);
+		std::cerr << "Error creating pipe" << std::endl;
+		exit(1);
+	}
+	int child_pid;
 	// fork for grep
-	if (fork() == 0)
+	if ((child_pid = fork()) == 0)
 	{
 		// close the read end of the pipe
 		close(pipe_grep_to_cut[PIPE_READ_END]);
-		// open the phonebook file for reading and dup it into stdin
-		dup2(open(PB_FILEPATH, O_RDONLY), STDIN_FILENO);
-		// dup the pipe write end into stdout
-		dup2(pipe_grep_to_cut[PIPE_WRITE_END], STDOUT_FILENO);
-		// execute the grep
-		execlp("grep", "grep", name, (char *) NULL);
+
+		// open the phonebook file for reading
+		int fd = open(PB_FILEPATH, O_RDONLY);
+		if (fd == -1)
+		{
+			strerror(errno);
+			std::cerr << "Error opening file" << std::endl;
+			exit(1);
+		}
+		// replaces stdin to the phonebook file
+		if (dup2(fd, STDIN_FILENO) == -1)
+		{
+			strerror(errno);
+			std::cerr << "Error redirecting stdin" << std::endl;
+			exit(1);
+		}
+
+		// redirect the pipe write end into stdout
+		if (dup2(pipe_grep_to_cut[PIPE_WRITE_END], STDOUT_FILENO) == -1)
+		{
+			strerror(errno);
+			std::cerr << "Error redirecting stdout" << std::endl;
+			exit(1);
+		}
+
+		// execute the grep on the received name
+		if (execlp("grep", "grep", name, (char *)NULL) < 0)
+		{
+			strerror(errno);
+			std::cerr << "Error executing grep" << std::endl;
+			exit(1);
+		}
 	}
-	
+
+	// wait for the child proccess to end
+	wait(&child_pid);
+
+	// check if the child proccess ended with an error (name not found)
+	if (WEXITSTATUS(child_pid) != 0)
+	{
+		return false;
+	}
+
 	// create the second pipe
-	pipe(pipe_cut_to_xargs);
+	if (pipe(pipe_cut_to_xargs) == -1)
+	{
+		strerror(errno);
+		std::cerr << "Error creating pipe" << std::endl;
+		exit(1);
+	}
+
 	if (fork() == 0)
 	{
 		// close unused pipe ends
 		close(pipe_grep_to_cut[PIPE_WRITE_END]);
 		close(pipe_cut_to_xargs[PIPE_READ_END]);
-		// dup the first pipe into stdin
-		dup2(pipe_grep_to_cut[PIPE_READ_END], STDIN_FILENO);
-		// dup the second pipe into stdout
-		dup2(pipe_cut_to_xargs[PIPE_WRITE_END], STDOUT_FILENO);
-		// execute cut
-		execlp("cut", "cut", "-d,", "-f2", (char *) NULL);
+
+		// replace stdin with the read end of the cut pipe
+		if (dup2(pipe_grep_to_cut[PIPE_READ_END], STDIN_FILENO) == -1)
+		{
+			strerror(errno);
+			std::cerr << "Error redirecting stdin" << std::endl;
+			exit(1);
+		}
+
+		// replace stdout with the write end of the xargs pipe
+		if (dup2(pipe_cut_to_xargs[PIPE_WRITE_END], STDOUT_FILENO) == -1)
+		{
+			strerror(errno);
+			std::cerr << "Error redirecting stdout" << std::endl;
+			exit(1);
+		}
+
+		// execute cut: delimiter is ',' and we want the second field (which contains the number)
+		if (execlp("cut", "cut", "-d,", "-f2", (char *)NULL) < 0)
+		{
+			strerror(errno);
+			std::cerr << "Error executing cut" << std::endl;
+			exit(1);
+		}
 	}
+
 	// close the unused pipe ends
 	close(pipe_grep_to_cut[PIPE_READ_END]);
 	close(pipe_grep_to_cut[PIPE_WRITE_END]);
 
 	// save the xargs pid
 	int xargs_pid;
+
 	// fork to run xargs
 	if ((xargs_pid = fork()) == 0)
 	{
 		// close the other end of the pipe
 		close(pipe_cut_to_xargs[PIPE_WRITE_END]);
+
 		// dup the read end into stdin
-		dup2(pipe_cut_to_xargs[PIPE_READ_END], STDIN_FILENO);
-		// execute xargs
-		execlp("xargs", "xargs", (char *) NULL);
+		if (dup2(pipe_cut_to_xargs[PIPE_READ_END], STDIN_FILENO) == -1)
+		{
+			strerror(errno);
+			std::cerr << "Error redirecting stdin" << std::endl;
+			exit(1);
+		}
+
+		// execute xargs - normalizes the output to be more readable (removes unnecessary spaces and newlines)
+		if (execlp("xargs", "xargs", (char *)NULL) < 0)
+		{
+			strerror(errno);
+			std::cerr << "Error executing xargs" << std::endl;
+			exit(1);
+		}
 	}
+
 	// close all remaining pipe ends
 	close(pipe_cut_to_xargs[PIPE_READ_END]);
 	close(pipe_cut_to_xargs[PIPE_WRITE_END]);
 
 	// wait until xargs finishes, to not mix outputs
 	waitpid(xargs_pid, NULL, 0);
+
+	// The name was found
 	return true;
 }
 
@@ -81,6 +171,10 @@ int main(int argc, char **argv)
 
 	char *name = argv[1];
 	std::cout << "Found number(s) for names matching \"" << name << "\": " << std::flush;
-	print_number(name);
+	
+	if(print_number(name) == false)
+	{
+		std::cout << "Name not found" << std::endl;
+	}
 	return 0;
 }
